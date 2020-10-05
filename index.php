1e98aad4f72b110d6f8242dd2957083d5ab34c41
Yesentooky <Yesentooky@gmail.com>
2020-10-06 04:42:13 +0500

    blocks added

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width,
         initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css\style.css">
+        <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <title>PHP веб сайт</title>
     </head>
@@ -20,20 +20,27 @@
         </div>
         <div class="container mt-5">
             <h3 class="mb-5">Наши статьи</h3>
-            <div class="card mb-4 shadow-sm">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Free</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>10 users included</li>
-                        <li>2 GB of storage</li>
-                        <li>Email support</li>
-                        <li>Help center access</li>
-                    </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                </div>
+
+            <div class="d-flex flex-wrap">
+                <?php
+                    for ($i = 1; $i < 7; $i++):
+                ?>
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">Просто текст</h4>
+                        </div>
+                        <div class="card-body">
+                            <img src="img/<?php echo $i ?>.jpg" alt="" class="img-thumbnail">
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>10 users included</li>
+                                <li>2 GB of storage</li>
+                                <li>Email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+                        </div>
+                    </div>
+                <?php endfor; ?>
             </div>
         </div>
     </body>
