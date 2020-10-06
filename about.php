d9b06655619d794ddc16592daefd20b9f3ecdd69
Yesentooky <Yesentooky@gmail.com>
2020-10-06 05:28:56 +0500

    Added contact form, implemented sending.

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <title>PHP веб сайт</title>
+    </head>
+    <body>
+        <?php require_once "blocks/header.php" ?>
+        <div class="container mt-5">
+            <h3>Контактная форма</h3>
+            <form action="check.php" method="post">
+                <input type="email" name="email" placeholder="Введите Email"
+                class="form-control"><br>
+                <textarea name="message" class="form-control"
+                placeholder="Введите ваше сообщение"></textarea><br>
+                <button type="submit" name="send" class="btn btn-success">Отправить</button>
+            </form>
+        </div>
+        <?php require_once "blocks/footer.php" ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,14 +10,7 @@
         <title>PHP веб сайт</title>
     </head>
     <body>
-        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 class="my-0 mr-md-auto font-weight-normal">YesTo</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
-                <a class="p-2 text-dark" href="#">Главная</a>
-                <a class="p-2 text-dark" href="#">Контакты</a>
-            </nav>
-            <a class="btn btn-outline-primary" href="#">Вход</a>
-        </div>
+        <?php require_once "blocks/header.php"; ?>
         <div class="container mt-5">
             <h3 class="mb-5">Наши статьи</h3>
 
@@ -43,5 +36,6 @@
                 <?php endfor; ?>
             </div>
         </div>
+        <?php require_once "blocks/footer.php" ?>
     </body>
 </html>
